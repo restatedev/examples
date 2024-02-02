c67f47905f2fbc733737d4b218a4af09a5e82290
Giselle van Dongen <giselle.vd@gmail.com>
2024-02-02 12:34:30 +0100

    Use companion object

--- scala/hello-world-http/src/main/scala/dev/restate/sdk/examples/Greeter.scala
+++ scala/hello-world-http/src/main/scala/dev/restate/sdk/examples/Greeter.scala
@@ -5,17 +5,20 @@ import dev.restate.sdk.common.{CoreSerdes, StateKey}
 import greeter.GreeterGrpc.GreeterImplBase
 import greeter.{GreetRequest, GreetResponse}
 import io.grpc.stub.StreamObserver
+import dev.restate.sdk.examples.Greeter.COUNT;
+
+object Greeter{
+  private val COUNT = StateKey.of[Integer]("count", CoreSerdes.INT)
+}
 
 class Greeter extends GreeterImplBase with RestateService {
-  private val COUNT = StateKey.of[Integer]("count", CoreSerdes.INT)
-
   override def greet(request: GreetRequest, responseObserver: StreamObserver[GreetResponse]): Unit = {
     val ctx = restateContext
 
     val count = ctx.get(COUNT).orElse(Integer.valueOf(1))
     ctx.set(COUNT, Integer.valueOf(count + 1))
 
-    responseObserver.onNext(GreetResponse.newBuilder.setMessage("Hello " + request.getName + " for the " + count + " time!").build)
+    responseObserver.onNext(GreetResponse.newBuilder.setMessage(s"Hello ${request.getName} for the ${count} time!").build)
     responseObserver.onCompleted()
   }
 }
