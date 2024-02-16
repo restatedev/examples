31f1fd93b27885239808d884e4b98de5880f9c97
Pavel Tcholakov <pavel@restate.dev>
2024-02-16 13:55:24 +0200

    Simplify retryable exception detection

--- typescript/promisify-anything/src/query-athena.ts
+++ typescript/promisify-anything/src/query-athena.ts
@@ -10,40 +10,47 @@ export default async function queryAthena(ctx: restate.RpcContext, query?: strin
   const requestId = ctx.rand.uuidv4(); // make up a stable idempotency token for calling Athena API
 
   const client = new athena.AthenaClient({});
-  const executionId = await ctx.sideEffect(async () => {
-    try {
-      const startQueryResult = await client.send(
-        new athena.StartQueryExecutionCommand({
-          QueryString: query ?? QUERY_SQL,
-          WorkGroup: "demo-workgroup",
-          ClientRequestToken: requestId,
-        }),
-      );
-      return startQueryResult.QueryExecutionId as string;
-    } catch (error) {
-      if (error instanceof athena.AthenaServiceException)
-        if (error.$fault === "client") throw new restate.TerminalError(error.message);
+  const startQueryOutput = await ctx.sideEffect(
+    terminateOnError(
+      (error) => (error instanceof athena.AthenaServiceException ? error.$fault === "client" : false),
+      async () =>
+        await client.send(
+          new athena.StartQueryExecutionCommand({
+            QueryString: query ?? QUERY_SQL,
+            WorkGroup: "demo-workgroup",
+            ClientRequestToken: requestId,
+          }),
+        ),
+    ),
+  );
+  const queryId = { QueryExecutionId: startQueryOutput.QueryExecutionId };
 
-      throw error; // rethrow other exceptions -- side effect retry policy will handle them
-    }
-  });
-  const queryId = { QueryExecutionId: executionId };
-
-  const state = await ctx.sideEffect(async () => {
-    const response = await client.send(new athena.GetQueryExecutionCommand(queryId));
-    const state = response.QueryExecution?.Status?.State;
-    if (!state || !isQueryStateFinal(state)) throw new Error("Non-final state"); // trigger retry of side effect
-    return state;
-  });
+  const state = await ctx.sideEffect(async () =>
+    assertFinalState((await client.send(new athena.GetQueryExecutionCommand(queryId))).QueryExecution?.Status?.State),
+  );
 
   if (state !== "SUCCEEDED") {
     throw new restate.TerminalError(`Query execution failed: ${state}. Please see logs for error details.`);
   }
 
   const result = await ctx.sideEffect(() => client.send(new athena.GetQueryResultsCommand(queryId)));
-  return { result: result.ResultSet, _id: executionId };
+  return { result: result.ResultSet, _id: startQueryOutput };
 }
 
-function isQueryStateFinal(state: athena.QueryExecutionState): boolean {
-  return ["SUCCEEDED", "FAILED", "CANCELLED"].includes(state);
+function terminateOnError<T>(isTerminal: (e: Error) => boolean, fn: () => Promise<T>): () => Promise<T> {
+  return async () => {
+    try {
+      return await fn();
+    } catch (e) {
+      if (e instanceof Error && isTerminal(e)) throw new restate.TerminalError(e.message);
+      throw e;
+    }
+  };
+}
+
+function assertFinalState(state?: athena.QueryExecutionState): athena.QueryExecutionState {
+  if (state == undefined || !["SUCCEEDED", "FAILED", "CANCELLED"].includes(state)) {
+    throw new Error(`Non-final state ${state ?? "undefined"}`);
+  }
+  return state;
 }
