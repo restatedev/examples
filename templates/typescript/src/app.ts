f764f0633c303136902c42134bf2f74220a671a9
igalshilman <igal@restate.dev>
2024-04-18 23:05:10 +0200

    [typescript] Migrate the template

--- templates/typescript/src/app.ts
+++ templates/typescript/src/app.ts
@@ -12,16 +12,19 @@ import * as restate from "@restatedev/restate-sdk";
 //
 // Have a look at the TS docs on the context, or at https://docs.restate.dev/
 //
-const sayHello = async (ctx: restate.Context, name: string) => {
+
+const hello = async (ctx: restate.Context, name: string) => {
   return `Hello ${name}!`;
 };
 
 // Create the Restate server to accept requests
 restate
   .endpoint()
-  .bindRouter(
-    "myservice", // the name of the service that serves the handlers
-    restate.router({ hello: sayHello }) // the routes and handlers in the service
+  .bind(
+    restate.service({
+      name: "myservice", // the service that serves the handlers
+      handlers: { hello },
+    })
   )
   .listen(9080);
 
@@ -31,14 +34,14 @@ restate
 //
 // Invoke this by calling Restate to invoke this handler durably:
 //
-//    curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/hello -d '{ "request": "Friend" }'
+//    curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/hello -d '"Friend"'
 //
 // To launch Restate and register this service (if you don't have Restate running already)
 //
 //  - On macOS:
 //    docker run --name restate_dev --rm -p 8080:8080 -p 9070:9070 -p 9071:9071 docker.io/restatedev/restate:latest
-//    curl -X POST http://localhost:9070/endpoints -H 'content-type: application/json' -d '{"uri": "http://host.docker.internal:9080"}'
+//    curl -X POST http://localhost:9070/deployments -H 'content-type: application/json' -d '{"uri": "http://host.docker.internal:9080"}'
 //
 //  - On Linux:
 //    docker run --name restate_dev --rm --network=host docker.io/restatedev/restate:latest
-//    curl -X POST http://localhost:9070/endpoints -H 'content-type: application/json' -d '{"uri": "http://localhost:9080"}'
+//    curl -X POST http://localhost:9070/deployments -H 'content-type: application/json' -d '{"uri": "http://localhost:9080"}'
