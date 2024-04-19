62e7edf43f8f5c4f20a2a545ca69555f558eaa3f
igalshilman <igal@restate.dev>
2024-04-20 00:45:46 +0200

    [typescript] Food ordering move th webui

--- end-to-end-applications/typescript/food-ordering/webui/src/services/sendToRestate.ts
+++ end-to-end-applications/typescript/food-ordering/webui/src/services/sendToRestate.ts
@@ -4,21 +4,43 @@ const RESTATE_HOST =
   process.env.REACT_APP_RESTATE_HOST || 'http://localhost:8080';
 const KAFKA_REST_PROXY_HOST = 'http://localhost:8088';
 
-const challenge = (): {challenge: string, challengeTime: string} => {
+const challenge = (): { challenge: string; challengeTime: string } => {
   const challengeTime = new Date().getTime();
-  const challenge = challengeTime ^ 7925119523126
-  return {challenge: challenge.toString(), challengeTime: challengeTime.toString()}
-}
+  const challenge = challengeTime ^ 7925119523126;
+  return {
+    challenge: challenge.toString(),
+    challengeTime: challengeTime.toString(),
+  };
+};
 
-export const sendRequestToRestate = async (
-  service: string,
-  method: string,
-  data: any
-) => {
+export const sendRequestToRestate = async ({
+  service,
+  method,
+  key,
+  data,
+  bg,
+}: {
+  service: string;
+  method: string;
+  data: any;
+  key?: string;
+  bg?: boolean;
+}) => {
   const c = challenge();
 
+  let url;
+  if (key) {
+    url = `${RESTATE_HOST}/${service}/${key}/${method}`;
+  } else {
+    url = `${RESTATE_HOST}/${service}/${method}`;
+  }
+
+  if (bg) {
+    url = url + '/send';
+  }
+
   return await (
-    await axios.post(`${RESTATE_HOST}/${service}/${method}`, data, {
+    await axios.post(url, data, {
       headers: {
         'Content-Type': 'application/json',
         'X-Challenge': c.challenge,
