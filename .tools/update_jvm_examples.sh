17efbee5a1ab69272c320c8bb825b6fd74d739db
Francesco Guardiani <francescoguard@gmail.com>
2024-04-22 11:23:41 +0200

    Add Java Maven template (#125)

--- .tools/update_jvm_examples.sh
+++ .tools/update_jvm_examples.sh
@@ -4,16 +4,20 @@ NEW_VERSION=$1
 SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
 PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
 
-function search_and_replace_version() {
+function search_and_replace_version_gradle() {
   sed -i 's/val restateVersion = "[0-9A-Z.-]*"/val restateVersion = "$NEW_VERSION"/' $1/build.gradle.kts
 }
 
-search_and_replace_version $PROJECT_ROOT/templates/java-gradle
-search_and_replace_version $PROJECT_ROOT/templates/kotlin-gradle
-search_and_replace_version $PROJECT_ROOT/templates/scala-sbt
+function search_and_replace_version_maven() {
+  pushd $1 && mvn versions:set-property -Dproperty=restate.version -DnewVersion=$NEW_VERSION && popd || exit
+}
 
-search_and_replace_version $PROJECT_ROOT/patterns-use-cases/sagas/sagas-java
+search_and_replace_version_gradle $PROJECT_ROOT/templates/java-gradle
+search_and_replace_version_maven $PROJECT_ROOT/templates/java-maven
+search_and_replace_version_gradle $PROJECT_ROOT/templates/kotlin-gradle
 
-search_and_replace_version $PROJECT_ROOT/tutorials/tour-of-restate-java
+search_and_replace_version_gradle $PROJECT_ROOT/patterns-use-cases/sagas/sagas-java
 
-search_and_replace_version $PROJECT_ROOT/end-to-end-applications/java/food-ordering/app
+search_and_replace_version_gradle $PROJECT_ROOT/tutorials/tour-of-restate-java
+
+search_and_replace_version_gradle $PROJECT_ROOT/end-to-end-applications/java/food-ordering/app
