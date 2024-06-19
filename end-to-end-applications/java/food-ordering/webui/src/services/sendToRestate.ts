bae7a02817f274419e000162c51334adfc76ea41
Stephan Ewen <stephan@restate.dev>
2024-06-20 00:22:16 +0200

    Remove Kafka for order submission, keep only for driver pings

--- end-to-end-applications/java/food-ordering/webui/src/services/sendToRestate.ts
+++ end-to-end-applications/java/food-ordering/webui/src/services/sendToRestate.ts
@@ -4,30 +4,16 @@ const RESTATE_HOST =
   process.env.REACT_APP_RESTATE_HOST || 'http://localhost:8080';
 const KAFKA_REST_PROXY_HOST = 'http://localhost:8088';
 
-const challenge = (): { challenge: string; challengeTime: string } => {
-  const challengeTime = new Date().getTime();
-  const challenge = challengeTime ^ 7925119523126;
-  return {
-    challenge: challenge.toString(),
-    challengeTime: challengeTime.toString(),
-  };
-};
-
-const challengeHeaders = () => {
-  const c = challenge();
-
-  return { 'X-Challenge': c.challenge, 'X-Challenge-Time': c.challengeTime };
-};
 
 export async function createOrder(orderId: string, order: any) {
   return await (
     await axios.post(
-      `${RESTATE_HOST}/OrderWorkflow/${orderId}/create`,
+      `${RESTATE_HOST}/OrderWorkflow/create/send`,
       order,
       {
         headers: {
           'content-type': 'application/json',
-          ...challengeHeaders(),
+          'idempotency-key': orderId
         },
       }
     )
@@ -36,9 +22,7 @@ export async function createOrder(orderId: string, order: any) {
 
 export async function getStatus(orderId: string) {
   return await (
-    await axios.get(`${RESTATE_HOST}/OrderStatusService/${orderId}/get`, {
-      headers: challengeHeaders(),
-    })
+    await axios.get(`${RESTATE_HOST}/OrderStatusService/${orderId}/get`, {})
   ).data;
 }
 
