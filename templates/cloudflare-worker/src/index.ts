6ca5d02f6d913db1ed3381847ec7d0b567dc6da5
Jack Kleeman <jackkleeman@gmail.com>
2024-06-21 14:54:57 +0200

    Use /fetch component

--- templates/cloudflare-worker/src/index.ts
+++ templates/cloudflare-worker/src/index.ts
@@ -1,8 +1,8 @@
-import { Context, endpoint, service } from "@restatedev/restate-sdk/cloudflare";
+import { Context, endpoint, service } from "@restatedev/restate-sdk/fetch";
 
 // Template of a Restate service and handler
 //
-// Have a look at the TS QuickStart to learn how to run this: https://docs.restate.dev/get_started/quickstart?sdk=ts
+// Have a look at the TS QuickStart: https://docs.restate.dev/get_started/quickstart?sdk=ts
 //
 
 const greet = async (ctx: Context, greeting: string) => {
