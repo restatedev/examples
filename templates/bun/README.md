65f0fec453b2829068ec538267150df082dfd724
Jack Kleeman <jackkleeman@gmail.com>
2024-06-24 12:48:17 +0100

    Add deno and bun examples (#156)

---
+++ templates/bun/README.md
@@ -0,0 +1,10 @@
+# Hello world - Bun example
+
+Sample project configuration of a Restate service using the TypeScript SDK and
+Bun.
+
+Have a look at the [TypeScript Quickstart guide](https://docs.restate.dev/get_started/quickstart?sdk=ts) for more information about the SDK.
+
+You can run locally with `npm run dev` and register to Restate with
+`restate dep add http://localhost:9080 --use-http1.1`. `--use-http1.1` is
+always needed with Bun, as it does not yet support HTTP2.
---
+++ templates/deno/main.ts
@@ -0,0 +1,19 @@
+import { Context, endpoint, service } from "npm:@restatedev/restate-sdk@^1.0.1/fetch";
+
+// Template of a Restate service and handler
+//
+// Have a look at the TS QuickStart: https://docs.restate.dev/get_started/quickstart?sdk=ts
+//
+
+const greeter = service({
+  name: "greeter",
+  handlers: {
+    greet: async (ctx: Context, greeting: string) => {
+      return `${greeting}!`;
+    },
+  },
+});
+
+const handler = endpoint().bind(greeter).handler();
+
+Deno.serve({ port: 9080 }, handler.fetch);
