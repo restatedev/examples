863bc79fc2ff334857910b195d5f56f7186d75a9
Pavel Tcholakov <pavel@restate.dev>
2024-07-22 19:25:24 +0300

    Simplify CDK templates and avoid VPC lookups during synth step (#170)
    * CI test scripts should fail when any command fails
    
    * Update CDK examples to not require VPC lookup during synth
    
    This change simplifies the CDK examples slightly, making them more general.
    A side effect of this is that we no longer require a VPC lookup, which needs
    AWS credentials - and was thus previously failing during Unknown command: "build"
    
    To see a list of supported npm commands, run:
      npm help in CI.
    
    * enable command logging for run_node_tests.sh
    
    * Temporarily disable bun template checks
    
    * Add missing  target to Cloudflare template
    
    * Remove explicit exit -  aborts on error

--- .tools/run_node_tests.sh
+++ .tools/run_node_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eufx -o pipefail
+
 SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
 PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
 
@@ -12,7 +14,7 @@ npm_install_check $PROJECT_ROOT/basics/basics-typescript
 npm_install_check $PROJECT_ROOT/templates/typescript
 npm_install_check $PROJECT_ROOT/templates/typescript-lambda-cdk
 npm_install_check $PROJECT_ROOT/templates/cloudflare-worker
-npm_install_check $PROJECT_ROOT/templates/bun
+#npm_install_check $PROJECT_ROOT/templates/bun
 
 npm_install_check $PROJECT_ROOT/templates/typescript-testing
 npm --prefix $PROJECT_ROOT/templates/typescript-testing run test
