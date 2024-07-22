a8a7974d57f2c7e6e2e6b30c4321db2ebd9f61c0
Pavel Tcholakov <pavel@restate.dev>
2024-07-22 15:02:32 +0200

    Remove explicit exit -  aborts on error

--- .tools/run_jvm_tests.sh
+++ .tools/run_jvm_tests.sh
@@ -5,19 +5,19 @@ set -eufx -o pipefail
 SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
 PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
 
-pushd $PROJECT_ROOT/templates/java-gradle && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/templates/java-maven && mvn verify && popd || exit
-pushd $PROJECT_ROOT/templates/kotlin-gradle && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/templates/kotlin-gradle-lambda-cdk/lambda && ./gradlew check && popd || exit
+pushd $PROJECT_ROOT/templates/java-gradle && ./gradlew check && popd
+pushd $PROJECT_ROOT/templates/java-maven && mvn verify && popd
+pushd $PROJECT_ROOT/templates/kotlin-gradle && ./gradlew check && popd
+pushd $PROJECT_ROOT/templates/kotlin-gradle-lambda-cdk/lambda && ./gradlew check && popd
 
-pushd $PROJECT_ROOT/basics/basics-java && ./gradlew check && popd || exit
+pushd $PROJECT_ROOT/basics/basics-java && ./gradlew check && popd
 
-pushd $PROJECT_ROOT/patterns-use-cases/sagas/sagas-java && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/patterns-use-cases/payment-state-machine/payment-state-machine-java && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/patterns-use-cases/async-signals-payment/async-signals-payment-java && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/patterns-use-cases/integrations/java-spring && ./gradlew check && popd || exit
+pushd $PROJECT_ROOT/patterns-use-cases/sagas/sagas-java && ./gradlew check && popd
+pushd $PROJECT_ROOT/patterns-use-cases/payment-state-machine/payment-state-machine-java && ./gradlew check && popd
+pushd $PROJECT_ROOT/patterns-use-cases/async-signals-payment/async-signals-payment-java && ./gradlew check && popd
+pushd $PROJECT_ROOT/patterns-use-cases/integrations/java-spring && ./gradlew check && popd
 
-pushd $PROJECT_ROOT/tutorials/tour-of-restate-java && ./gradlew check && popd || exit
+pushd $PROJECT_ROOT/tutorials/tour-of-restate-java && ./gradlew check && popd
 
-pushd $PROJECT_ROOT/end-to-end-applications/java/food-ordering/app && ./gradlew check && popd || exit
-pushd $PROJECT_ROOT/end-to-end-applications/kotlin/food-ordering/app && ./gradlew check && popd || exit
+pushd $PROJECT_ROOT/end-to-end-applications/java/food-ordering/app && ./gradlew check && popd
+pushd $PROJECT_ROOT/end-to-end-applications/kotlin/food-ordering/app && ./gradlew check && popd
