8dda7a4be019679e19ae86685cef8cc0d705f1b4
Giselle van Dongen <giselle.vd@gmail.com>
2024-09-12 08:11:20 +0200

    Create zip for Tour of Restate Python

--- .tools/prepare_release_zip.sh
+++ .tools/prepare_release_zip.sh
@@ -33,6 +33,7 @@ create_release_zip templates/python python-hello-world
 create_release_zip tutorials/tour-of-restate-go go-tour-of-restate
 create_release_zip tutorials/tour-of-restate-java java-tour-of-restate
 create_release_zip tutorials/tour-of-restate-typescript typescript-tour-of-restate
+create_release_zip tutorials/tour-of-restate-python python-tour-of-restate
 
 create_release_zip patterns-use-cases/payment-state-machine/payment-state-machine-typescript typescript-payment-state-machine
 
