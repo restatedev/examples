45d6dcf690c6e7dd42a8702368e52447d08da350
Giselle van Dongen <giselle.vd@gmail.com>
2024-11-21 14:42:10 +0100

    Use pydantic for Python template

--- templates/python/example.py
+++ templates/python/example.py
@@ -1,23 +1,22 @@
 import uuid
-from datetime import timedelta
-
-from restate import Service, Context
 import restate
-
+from datetime import timedelta
+from restate import Service, Context
+from pydantic_models import GreetingRequest, Greeting
 from utils import send_notification, send_reminder
 
 greeter = Service("Greeter")
 
 
 @greeter.handler()
-async def greet(ctx: Context, name: str) -> str:
+async def greet(ctx: Context, req: GreetingRequest) -> Greeting:
     # Durably execute a set of steps; resilient against failures
     greeting_id = await ctx.run("generate UUID", lambda: str(uuid.uuid4()))
-    await ctx.run("send notification", lambda: send_notification(greeting_id, name))
+    await ctx.run("send notification", lambda: send_notification(greeting_id, req.name))
     await ctx.sleep(timedelta(seconds=1))
     await ctx.run("send reminder", lambda: send_reminder(greeting_id))
 
     # Respond to caller
-    return f"You said hi to {name}!"
+    return Greeting(message=f"You said hi to {req.name}!")
 
 app = restate.app(services=[greeter])
