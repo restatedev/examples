097ff975f649e4840cab561d890fe8e35cbe6ee4
Giselle van Dongen <giselle.vd@gmail.com>
2024-12-20 10:44:03 +0100

    Remove restate from dev dependencies

--- typescript/patterns-use-cases/async-tasks-payment-signals/src/utils/stripe_utils.ts
+++ typescript/patterns-use-cases/async-tasks-payment-signals/src/utils/stripe_utils.ts
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2024 - Restate Software, Inc., Restate GmbH
- *
- * This file is part of the Restate SDK for Node.js/TypeScript,
- * which is released under the MIT license.
- *
- * You can find a copy of the license in file LICENSE in the root
- * directory of this repository or package, or at
- * https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
- */
-
 import { TerminalError } from "@restatedev/restate-sdk";
 import Stripe from "stripe";
 
