ab5fc8828a50f9b948a36a5e3afdb9696614baaa
Giselle van Dongen <giselle.vd@gmail.com>
2025-01-03 15:19:47 +0100

    Add extra comments to go eventenrichment example

--- go/patterns-use-cases/src/eventenrichment/packagetracker.go
+++ go/patterns-use-cases/src/eventenrichment/packagetracker.go
@@ -21,13 +21,19 @@ type PackageInfo struct {
 
 const PACKAGE_INFO = "package-info"
 
+// Package tracking system:
+// Digital twin representing a package in delivery with real-time location updates.
+// Handlers get called over HTTP or Kafka.
+
 type PackageTracker struct{}
 
+// RegisterPackage Called first by the seller over HTTP
 func (PackageTracker) RegisterPackage(ctx restate.ObjectContext, packageInfo PackageInfo) error {
 	restate.Set[PackageInfo](ctx, "package-info", packageInfo)
 	return nil
 }
 
+// UpdateLocation Connected to a Kafka topic for real-time location updates
 func (PackageTracker) UpdateLocation(ctx restate.ObjectContext, locationUpdate LocationUpdate) error {
 	packageInfo, err := restate.Get[PackageInfo](ctx, PACKAGE_INFO)
 	if err != nil {
@@ -38,11 +44,13 @@ func (PackageTracker) UpdateLocation(ctx restate.ObjectContext, locationUpdate L
 		return restate.TerminalError(errors.New("package not found"))
 	}
 
+	// Update the package details in the state
 	packageInfo.Locations = append(packageInfo.Locations, locationUpdate)
 	restate.Set[PackageInfo](ctx, PACKAGE_INFO, packageInfo)
 	return nil
 }
 
+// GetPackageInfo Called by the delivery dashboard to get the package details
 func (PackageTracker) GetPackageInfo(ctx restate.ObjectSharedContext) (PackageInfo, error) {
 	packageInfo, err := restate.Get[PackageInfo](ctx, PACKAGE_INFO)
 	if err != nil {
@@ -60,3 +68,10 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+// Process package tracking events via HTTP:
+/*
+curl localhost:8080/PackageTracker/package1234/RegisterPackage -H 'content-type: application/json' -d '{ "finalDestination": "Bridge 6, Amsterdam", "locations": [] }'
+curl localhost:8080/PackageTracker/package1234/UpdateLocation -H 'content-type: application/json' -d '{ "timestamp": "2024-12-11T12:00:00Z", "location": "Warehouse A" }'
+curl localhost:8080/PackageTracker/package1234/GetPackageInfo
+*/
