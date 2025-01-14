2003b5328ad86acd7e876328b272236f1a22e4b6
Giselle van Dongen <giselle.vd@gmail.com>
2025-01-14 18:00:20 +0100

    Cleanup basics rust

--- rust/basics/src/p3_workflows.rs
+++ rust/basics/src/p3_workflows.rs
@@ -3,28 +3,45 @@ mod stubs;
 use restate_sdk::prelude::*;
 use crate::stubs::{create_user_entry, send_email_with_link, User};
 
+// Workflow are a special type of Virtual Object with a run handler that runs once per ID.
+// Workflows are stateful and can be interacted with via queries (getting data out of the workflow)
+// and signals (pushing data to the workflow).
+//
+// Workflows are used to model long-running flows, such as user onboarding, order processing, etc.
+// Workflows have the following handlers:
+//  - Main workflow in run() method
+//  - Additional methods interact with the workflow.
+// Each workflow instance has a unique ID and runs only once (to success or failure).
+
 #[restate_sdk::workflow]
-pub trait SignupWorkflow {
+trait SignupWorkflow {
     async fn run(user: Json<User>) -> Result<bool, HandlerError>;
     #[shared]
     async fn click(secret: String) -> Result<(), HandlerError>;
 }
 
-pub struct SignupWorkflowImpl;
+struct SignupWorkflowImpl;
 
 impl SignupWorkflow for SignupWorkflowImpl {
+    // --- The workflow logic ---
     async fn run(&self, mut ctx: WorkflowContext<'_>, Json(user): Json<User>) -> Result<bool, HandlerError> {
 
+        // Durably executed action; write to other system
         ctx.run(|| create_user_entry(&user)).await?;
 
+        // Send the email with the verification link
         let secret = ctx.rand_uuid().to_string();
+        // workflow ID = user ID; workflow runs once per user
         let user_id = ctx.key();
         ctx.run(|| send_email_with_link(user_id, &user, &secret)).await?;
 
+        // Wait until user clicked email verification link
+        // Promise gets resolved or rejected by the other handlers
         let click_secret = ctx.promise::<String>("email-link").await?;
         Ok(click_secret == secret)
     }
 
+    // --- Other handlers interact with the workflow via queries and signals ---
     async fn click(&self, ctx: SharedWorkflowContext<'_>, secret: String) -> Result<(), HandlerError> {
         ctx.resolve_promise::<String>("email-link", secret);
         Ok(())
