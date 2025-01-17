d0c9a54bf30f36861224459073b67b2e4ed7f200
Giselle van Dongen <giselle.vd@gmail.com>
2025-01-17 15:57:42 +0100

    Add note on endpoint to webhook example

--- typescript/patterns-use-cases/src/webhookcallbacks/webhook_callback_router.ts
+++ typescript/patterns-use-cases/src/webhookcallbacks/webhook_callback_router.ts
@@ -5,7 +5,8 @@ const webhookCallbackRouter = restate.service({
     name : "WebhookCallbackRouter",
     handlers: {
         // Any handler can be a durable webhook processor that never loses events
-        // You don't need to do anything special for this. Just point your webhook to the handler endpoint.
+        // You don't need to do anything special for this.
+        // Just point your webhook to the handler endpoint: restate:8080/WebhookCallbackRouter/onStripeEvent
         onStripeEvent: async (ctx: restate.Context, event: StripeEvent) => {
             if (event.type === "invoice.payment_failed") {
                 ctx.objectSendClient(PaymentTracker, event.data.object.id)
