59c58bf12907c53eb071b1f01359ca5ffc1b5e97
Kevin Webb <kpwebb@gmail.com>
2025-01-28 12:54:19 -0500

    Add simulated failure handler

--- rust/templates/rust/src/main.rs
+++ rust/templates/rust/src/main.rs
@@ -9,20 +9,41 @@ trait Greeter {
     async fn greet(name: String) -> Result<String, HandlerError>;
 }
 
+#[restate_sdk::service]
+trait GreeterFailure {
+    async fn greet(name: String) -> Result<String, HandlerError>;
+}
+
+
 struct GreeterImpl;
+struct GreeterFailureImpl;
 
 impl Greeter for GreeterImpl {
     async fn greet(&self, mut ctx: Context<'_>, name: String) -> Result<String, HandlerError> {
         // Durably execute a set of steps; resilient against failures
         let greeting_id = ctx.rand_uuid().to_string();
-        ctx.run(|| send_notification(&greeting_id, &name)).await?;
+        ctx.run(|| send_notification(&greeting_id, &name, false)).await?;
         ctx.sleep(Duration::from_millis(1000)).await?;
-        ctx.run(|| send_reminder(&greeting_id)).await?;
+        ctx.run(|| send_reminder(&greeting_id, false)).await?;
 
         // Respond to caller
         Ok(format!("Greetings {name}"))
     }
 }
+
+impl GreeterFailure for GreeterFailureImpl {
+    async fn greet(&self, mut ctnx: Context<'_>, name: String) -> Result<String, HandlerError> {
+        // Durably execute a set of steps; resilient against failures
+        let greeting_id = ctx.rand_uuid().to_string();
+        ctx.run(|| send_notification(&greeting_id, &name, true)).await?;
+        ctx.sleep(Duration::from_millis(1000)).await?;
+        ctx.run(|| send_reminder(&greeting_id, true)).await?;
+
+        // Respond to caller
+        Ok(format!("Greetings {name}"))
+    }
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -32,6 +53,7 @@ async fn main() {
     HttpServer::new(
         Endpoint::builder()
             .bind(GreeterImpl.serve())
+            .bind(GreeterFailureImpl.serve())
             .build(),
     )
         .listen_and_serve("0.0.0.0:9080".parse().unwrap())
