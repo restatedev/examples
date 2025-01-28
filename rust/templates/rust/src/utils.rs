5d15d3275ed347198f4d8e684e70079da8edc0a2
Kevin Webb <kpwebb@gmail.com>
2025-01-28 12:54:56 -0500

    add simulated failure flag and error message

--- rust/templates/rust/src/utils.rs
+++ rust/templates/rust/src/utils.rs
@@ -2,19 +2,19 @@ use rand::random;
 use anyhow::{anyhow, Result};
 use restate_sdk::errors::HandlerError;
 
-pub async fn send_notification(greeting_id: &str, name: &str) -> Result<(), HandlerError> {
-    if random::<f32>() < 0.5 {
-        println!("👻 Failed to send notification: {} - {}", greeting_id, name);
-        return Err(HandlerError::from(anyhow!("Failed to send notification: {} - {}", greeting_id, name)));
+pub async fn send_notification(greeting_id: &str, name: &str, simulate_faulure:bool) -> Result<(), HandlerError> {
+    if simulate_faulure && random::<f32>() < 0.5 {
+        println!("[👻 SIMULATED] Failed to send notification: {} - {}", greeting_id, name);
+        return Err(HandlerError::from(anyhow!("[SIMULATED] Failed to send notification: {} - {}", greeting_id, name)));
     }
     println!("Notification sent: {} - {}", greeting_id, name);
     Ok(())
 }
 
-pub async fn send_reminder(greeting_id: &str)-> Result<(), HandlerError> {
-    if random::<f32>() < 0.5 {
-        println!("👻 Failed to send reminder:  - {}", greeting_id);
-        return Err(HandlerError::from(anyhow!("Failed to send reminder: {}", greeting_id)));
+pub async fn send_reminder(greeting_id: &str, simulate_faulure:bool)-> Result<(), HandlerError> {
+    if simulate_faulure && random::<f32>() < 0.5 {
+        println!("[👻 SIMULATED] Failed to send reminder:  - {}", greeting_id);
+        return Err(HandlerError::from(anyhow!("[SIMULATED] Failed to send reminder: {}", greeting_id)));
     }
     println!("Reminder sent: {}", greeting_id);
     Ok(())
