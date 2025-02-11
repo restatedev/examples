e5be8ab143f71241adecc43846aae728b87b1913
slinkydeveloper <francescoguard@gmail.com>
2025-02-11 15:33:16 +0100

    Update Python SDK to 0.5.0

--- .tools/update_python_examples.sh
+++ .tools/update_python_examples.sh
@@ -12,6 +12,8 @@ function search_and_replace_version() {
 }
 
 search_and_replace_version $PROJECT_ROOT/python/templates/python
+search_and_replace_version $PROJECT_ROOT/python/basics
+search_and_replace_version $PROJECT_ROOT/python/patterns-use-cases
 search_and_replace_version $PROJECT_ROOT/python/tutorials/tour-of-restate-python
 search_and_replace_version $PROJECT_ROOT/python/end-to-end-applications/rag-ingestion
 search_and_replace_version $PROJECT_ROOT/python/end-to-end-applications/food-ordering/app
