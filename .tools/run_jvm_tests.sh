2e8f6e3421d073cd63a87880a3dd27edf9971042
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-06 13:04:30 +0100

    Fix jvm tests

--- .tools/run_jvm_tests.sh
+++ .tools/run_jvm_tests.sh
@@ -23,6 +23,6 @@ pushd $PROJECT_ROOT/java/tutorials/tour-of-restate-java && ./gradlew --console=p
 
 pushd $PROJECT_ROOT/java/end-to-end-applications/food-ordering/app && ./gradlew --console=plain check && popd
 pushd $PROJECT_ROOT/java/end-to-end-applications/subway-fare-calculator && ./gradlew --console=plain check && popd
-pushd $PROJECT_ROOT/java/end-to-end-applications/workflow-interpreter && ./gradlew --console=plain check && popd
+pushd $PROJECT_ROOT/java/end-to-end-applications/workflow-interpreter && mvn verify && popd
 pushd $PROJECT_ROOT/kotlin/end-to-end-applications/food-ordering/app && ./gradlew --console=plain check && popd
 pushd $PROJECT_ROOT/kotlin/end-to-end-applications/kmp-android-todo-app && ./gradlew --console=plain check && popd
