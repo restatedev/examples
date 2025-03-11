143a7d38dd25d0cfac2d0da4478392b530972668
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-11 17:49:03 +0100

    Update UI name

--- python/end-to-end-applications/chat-bot/ui/app/layout.tsx
+++ python/end-to-end-applications/chat-bot/ui/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Restate AI chatbot',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
