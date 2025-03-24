a63094dfd47f2f74b3c1d4e90cd5fcf2fa120513
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-24 10:05:37 +0100

    Add comments back

--- go/patterns-use-cases/src/eventenrichment/packagetracker.go
+++ go/patterns-use-cases/src/eventenrichment/packagetracker.go
@@ -18,13 +18,19 @@ type PackageInfo struct {
 	Locations        []LocationUpdate `json:"locations"`
 }
 
+// Package tracking system:
+// Digital twin representing a package in delivery with real-time location updates.
+// Handlers get called over HTTP or Kafka.
+
 type PackageTracker struct{}
 
+// RegisterPackage Called first by the seller over HTTP
 func (PackageTracker) RegisterPackage(ctx restate.ObjectContext, packageInfo PackageInfo) error {
 	restate.Set[PackageInfo](ctx, "package-info", packageInfo)
 	return nil
 }
 
+// UpdateLocation Connected to a Kafka topic for real-time location updates
 func (PackageTracker) UpdateLocation(ctx restate.ObjectContext, locationUpdate LocationUpdate) error {
 	packageInfo, err := restate.Get[*PackageInfo](ctx, "package-info")
 	if err != nil {
@@ -34,11 +40,13 @@ func (PackageTracker) UpdateLocation(ctx restate.ObjectContext, locationUpdate L
 		return restate.TerminalError(errors.New("package not found"))
 	}
 
+	// Update the package details in the state
 	packageInfo.Locations = append(packageInfo.Locations, locationUpdate)
 	restate.Set[PackageInfo](ctx, "package-info", *packageInfo)
 	return nil
 }
 
+// GetPackageInfo Called by the delivery dashboard to get the package details
 func (PackageTracker) GetPackageInfo(ctx restate.ObjectSharedContext) (*PackageInfo, error) {
 	return restate.Get[*PackageInfo](ctx, "package-info")
 }
