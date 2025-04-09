8abe3bd7228ba0bc4e53ee05299b6d73b47cbb64
slinkydeveloper <francescoguard@gmail.com>
2025-04-09 21:41:02 +0200

    For now remove the state test, this doesn't seem to work.

--- typescript/templates/typescript-testing/test/test.ts
+++ typescript/templates/typescript-testing/test/test.ts
@@ -1,9 +1,10 @@
 import { RestateTestEnvironment } from "@restatedev/restate-sdk-testcontainers";
 import { exampleService } from "../src/example_service";
 import { exampleObject } from "../src/example_object";
+import { setTimeout } from "node:timers/promises";
 import * as clients from "@restatedev/restate-sdk-clients";
 
-describe("ExampleObject", () => {
+describe("ExampleService", () => {
     let restateTestEnvironment: RestateTestEnvironment;
     let restateIngress: clients.Ingress;
 
@@ -50,8 +51,21 @@ describe("ExampleObject", () => {
         const state = restateTestEnvironment.stateOf(exampleObject, "Sarah");
         expect(await state.getAll()).toStrictEqual({})
         expect(await state.get("count")).toBeNull();
-        
-        await state.set("count", 123)
+
+        // Setting state is an eventually consistent operation, so retrying might be needed
+        let i = 0;
+        while(true) {
+            try {
+                await state.set("count", 123)
+                break
+            } catch (e) {
+                await setTimeout(1000)
+                i++
+                if (i > 10) {
+                    throw new Error("Unable to set state 'count'")
+                }
+            }
+        }
         const greet = await restateIngress.objectClient(exampleObject, "Sarah")
             .greet();
 
