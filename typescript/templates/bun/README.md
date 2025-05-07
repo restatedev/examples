8da33078cc80918b0a5d532885e1056aa165d2e4
Jack Kleeman <jackkleeman@gmail.com>
2025-05-07 09:12:40 +0100

    Use node http2 server with Bun

--- typescript/templates/bun/README.md
+++ typescript/templates/bun/README.md
@@ -6,5 +6,4 @@ Bun.
 Have a look at the [TypeScript Quickstart guide](https://docs.restate.dev/get_started/quickstart?sdk=ts) for more information about this template.
 
 You can run locally with `npm run dev` and register to Restate with
-`restate dep add http://localhost:9080 --use-http1.1`. `--use-http1.1` is
-always needed with Bun, as it does not yet support HTTP2.
+`restate dep add http://localhost:9080`.
--- typescript/templates/bun/src/index.ts
+++ typescript/templates/bun/src/index.ts
@@ -1,15 +1,14 @@
-import * as restate from "@restatedev/restate-sdk/fetch";
-import {sendNotification, sendReminder} from "./utils";
+import * as restate from "@restatedev/restate-sdk";
+import { sendNotification, sendReminder } from "./utils.ts";
 
-const handler = restate
+restate
   .endpoint()
   .bind(
     restate.service({
       name: "Greeter",
       handlers: {
         greet: async (ctx: restate.Context, name: string) => {
-
-            // Durably execute a set of steps; resilient against failures
+          // Durably execute a set of steps; resilient against failures
           const greetingId = ctx.rand.uuidv4();
           await ctx.run(() => sendNotification(greetingId, name));
           await ctx.sleep(1000);
@@ -21,11 +20,4 @@ const handler = restate
       },
     }),
   )
-  .handler();
-
-const server = Bun.serve({
-  port: 9080,
-  ...handler,
-});
-
-console.log(`Listening on ${server.url}`);
+  .listen(9080);
