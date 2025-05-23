f0294312b91696cb167502205e11efc574eeebe0
Nik Nasr <nik@restate.dev>
2025-05-23 17:32:29 +0100

    update more packages
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- typescript/end-to-end-applications/ai-image-workflows/src/types/types.ts
+++ typescript/end-to-end-applications/ai-image-workflows/src/types/types.ts
@@ -1,6 +1,8 @@
+import { ImageFormat } from "puppeteer";
+
 export type WorkflowStep = {
     imgInputPath?: string;
-    imgOutputPath?: string;
+    imgOutputPath?: `${string}.${ImageFormat}`;
     action: string;
     parameters: any;
 }
