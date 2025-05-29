1fe48f1cefdf550417067fb4df60ee37b2200235
Giselle van Dongen <giselle@restate.dev>
2025-05-29 17:39:11 +0200

    Simplify python sagas

--- python/patterns-use-cases/sagas/activities/car_rental_client.py
+++ python/patterns-use-cases/sagas/activities/car_rental_client.py
@@ -1,12 +1,8 @@
 import logging
-import uuid
 
 from pydantic import BaseModel
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] [%(process)d] [%(levelname)s] - %(message)s",
-)
+logger = logging.getLogger(__name__)
 
 
 class CarRentalRequest(BaseModel):
@@ -14,22 +10,13 @@ class CarRentalRequest(BaseModel):
     rental_date: str
 
 
-async def reserve(request: CarRentalRequest) -> str:
+async def book(customer_id: str, request: CarRentalRequest):
     # this should implement the communication with the rental
     # provider's APIs
-    # just return a mock random id representing the reservation
-    booking_id = str(uuid.uuid4())
-    logging.info(f"Car rental reservation created with id: {booking_id}")
-    return booking_id
+    logger.info(f"Car rental reservation created for customer: {customer_id}")
 
 
-async def confirm(booking_id: str):
+async def cancel(customer_id: str):
     # this should implement the communication with the rental
     # provider's APIs
-    logging.info(f"Car rental reservation confirmed with id: {booking_id}")
-
-
-async def cancel(booking_id: str):
-    # this should implement the communication with the rental
-    # provider's APIs
-    logging.info(f"Car rental reservation cancelled with id: {booking_id}")
+    logger.info(f"Car rental reservation cancelled for customer: {customer_id}")
--- python/patterns-use-cases/sagas/activities/flight_client.py
+++ python/patterns-use-cases/sagas/activities/flight_client.py
@@ -1,32 +1,22 @@
 import logging
-import uuid
 
 from pydantic import BaseModel
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] [%(process)d] [%(levelname)s] - %(message)s",
-)
 logger = logging.getLogger(__name__)
 
 
-class FlightBookingRequest(BaseModel):
+class FlightRequest(BaseModel):
     flight_id: str
     passenger_name: str
 
 
-async def reserve(request: FlightBookingRequest) -> str:
-    """Reserves a flight."""
-    booking_id = str(uuid.uuid4())
-    logger.info(f"Flight reservation created with id: {booking_id}")
-    return booking_id
+async def book(customer_id: str, request: FlightRequest):
+    # this should implement the communication with the flight
+    # provider's APIs
+    logging.info(f"Flight reservation created for customer: {customer_id}")
 
 
-async def confirm(flight_booking_id: str):
-    """Confirms a flight booking."""
-    logger.info(f"Flight reservation confirmed with id: {flight_booking_id}")
-
-
-async def cancel(flight_booking_id: str):
-    """Cancels a flight booking."""
-    logger.info(f"Flight reservation cancelled with id: {flight_booking_id}")
+async def cancel(customer_id: str):
+    # this should implement the communication with the flight
+    # provider's APIs
+    logging.info(f"Flight reservation cancelled for customer: {customer_id}")
