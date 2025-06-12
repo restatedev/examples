1e203816d3c985e7b6aeaf3cc7641643bd393597
Giselle van Dongen <giselle.vd@gmail.com>
2025-06-12 13:12:52 +0200

    Add email verification workflow (#297)
    * Add email verification workflow
    
    * Fix links
    
    * Format

---
+++ java/patterns-use-cases/src/main/java/my/example/concurrenttasks/utils/EmailClient.java
@@ -0,0 +1,26 @@
+package my.example.concurrenttasks.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EmailClient {
+  private static final Logger logger = LogManager.getLogger(EmailClient.class);
+
+  public static String sendVerificationEmail(String email, String id) {
+    logger.info("Sending email to {}", email);
+    logger.info(
+        "\n Verify by clicking: curl http://localhost:8080/restate/awakeables/"
+            + id
+            + "/resolve --json 'true'");
+
+    return "Email sent";
+  }
+
+  public static void sendReminder(String email, String id) {
+    logger.info("Sending reminder to {}", email);
+    logger.info(
+        "\n Verify by clicking: curl http://localhost:8080/restate/awakeables/"
+            + id
+            + "/resolve --json 'true'");
+  }
+}
