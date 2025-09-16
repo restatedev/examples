c159ad7e47c9472be05191087dc4437d6b15558d
Giselle van Dongen <giselle.vd@gmail.com>
2025-09-16 12:24:54 +0200

    Update java/tutorials/tour-of-orchestration-java/src/main/java/my/example/objects/UserSubscriptions.java
    Co-authored-by: Francesco Guardiani <francescoguard@gmail.com>

--- java/tutorials/tour-of-orchestration-java/src/main/java/my/example/objects/UserSubscriptions.java
+++ java/tutorials/tour-of-orchestration-java/src/main/java/my/example/objects/UserSubscriptions.java
@@ -32,6 +32,6 @@ public class UserSubscriptions {
 
   @Shared
   public Set<String> getSubscriptions(SharedObjectContext ctx) {
-    return ctx.get(SUBSCRIPTIONS).orElse(new HashSet<>());
+    return ctx.get(SUBSCRIPTIONS).orElse(Set.of());
   }
 }
