093953f87375519b42e2e72238faf02f3b4df0bb
slinkydeveloper <me@slinkydeveloper.com>
2025-09-24 14:55:53 +0200

    Simplify nextjs template

--- typescript/templates/nextjs/components/Badge.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export function Badge({ children }: PropsWithChildren<unknown>) {
-  return (
-    <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-      {children}
-    </span>
-  );
-}
--- typescript/templates/nextjs/restate/serve.ts
+++
@@ -1,8 +0,0 @@
-import { createEndpointHandler } from "@restatedev/restate-sdk/fetch";
-
-export const serveRestate = (fetch: ReturnType<typeof createEndpointHandler>) => {
-  return {
-    POST: (req: Request) => fetch(req),
-    GET: (req: Request) => fetch(req),
-  };
-};
